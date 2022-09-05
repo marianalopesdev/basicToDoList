198394dbe3511a1ab1d4dd6e14b1532369757e9d
marianalopesdev <52184708+marianalopesdev@users.noreply.github.com>
2022-09-05 03:04:22 -0300

    delete tasks feature added

--- script.js
+++ script.js
@@ -14,22 +14,35 @@ function addNewTask(e) {
     tasks.push(task);
     populateList(tasks, taskList);
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    this.reset();;
+    this.reset();
 }
 
 function populateList(tasks = [], taskList) {
     taskList.innerHTML = tasks.map((task, i) => {
         return `
     <li>
-      <input type="checkbox" name="check" data-index=${i} id="task${i}" ${task.status ? 'checked' : ''} />
+      <input type="checkbox" data-index=${i} id="task${i}" ${task.status ? 'checked' : ''} />
       <label for="task${i}">${task.taskTitle}</label>
       <label for="task${i}">${task.status}</label>     
-      <a href="" onclick="pup(e)" data-index=${i} id="task${i}">edit</a>     
+      <a href="" onclick="pup(this)" data-index=${i} id="task${i}">edit</a> 
+     
     
      <div id="lines"></div>
      <button type="button" onclick="addInput('lines')">+</button>
+     <button type="button" data-index=${i} onclick="deleteTask(this)">X</button>
     `;
     }).join('');
+}
+
+function deleteTask(e){
+    
+    const taskIndex = e.dataset.index;
+
+    if (taskIndex > -1) {
+        tasks.splice(taskIndex, 1);
+    }
+
+    saveOnLocalStorage();
 }
 
 function toggleDone(e) {
@@ -50,6 +63,7 @@ function toggle(source) {
                 checkboxes[i].checked = source.checked;
                 tasks[i].status = source.checked;
             }
+
         saveOnLocalStorage();
     }
 }
@@ -63,22 +77,20 @@ populateList(tasks, taskList);
 
 addTasks.addEventListener('submit', addNewTask);
 taskList.addEventListener('click', toggleDone);
-taskList.addEventListener('click', pup);
+//taskList.addEventListener('click', pup);
 // markAllTasks.addEventListener('click', markAll);
 // unmarkAllTasks.addEventListener('click', unmarkAll);
 
+
+
 function pup(e) {
     console.log('pup');
-    const targetClick = e.target;
-    const taskIndex = targetClick.dataset.index;
-    tasks[taskIndex].status = !tasks[taskIndex].status;
-    var elemento = document.getElementById("task1${i}");
-    elemento.dis
-    
-
-    saveOnLocalStorage();
-    populateList(tasks, taskList);
-
+   // const targetClick = e.target;
+    const taskIndex = e.dataset.index;
+   // tasks[taskIndex].status = !tasks[taskIndex].status;
+    taskList.innerHTML = `<input type="text" id="tasken" placeholder="insert do pup" required>`;
+    var b = document.getElementById("task${i}");
+    console.log(taskIndex);
 }
 
 
@@ -86,8 +98,11 @@ var formLine = 1;
 function addInput(divName) {
 	var newdiv = document.createElement('div');
 	newdiv.innerHTML  = '['+formLine+']';
-	newdiv.innerHTML += '<input type="text" id="task${i}" class="bubu" placeholder="Insert k" required>';
-	
+	newdiv.innerHTML = '<input type="text" name="xuxu" placeholder="Insert add input" class="taskListV"i required>';
+	// var y = taskList.getElementsByName("xuxu");
+    
+    // console.log(y);
+    // y.classList.add("taskListV"); 
 	document.getElementById(divName).appendChild(newdiv);
 	formLine++;
 }
