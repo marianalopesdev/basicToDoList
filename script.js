0a300d55314a56734197c045044d5b56c1996e0e
marianalopesdev <52184708+marianalopesdev@users.noreply.github.com>
2022-09-07 05:18:36 -0300

    code refactoring and style changes

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const addTasks = document.querySelector('.add-tasks');
 const taskList = document.querySelector('.taskList');
-// const markAllTasks = document.querySelector('.markAll');
-// const unmarkAllTasks = document.querySelector('.unmarkAll');
+const updateInput = document.getElementsByName("updateInput");
+const updateButton = document.getElementsByName("updateButton");
+const deleteButton = document.getElementsByName("deleteButton");
+const editButton = document.getElementsByName("editButton");
+
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+populateTaskList(tasks, taskList);
+
+addTasks.addEventListener('submit', addNewTask);
+taskList.addEventListener('change', toggleDone);
 
 function addNewTask(e) {
     e.preventDefault();
@@ -12,46 +20,35 @@ function addNewTask(e) {
         status: false
     };
     tasks.push(task);
-    populateList(tasks, taskList);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    populateTaskList(tasks, taskList);
+    saveOnLocalStorage();
     this.reset();
 }
 
-function populateList(tasks = [], taskList) {
+function populateTaskList(tasks = [], taskList) {
     taskList.innerHTML = tasks.map((task, i) => {
         return `
-    <li>
-      <input type="checkbox" data-index=${i} id="task${i}" ${task.status ? 'checked' : ''} />
-      <label for="task${i}" contentEditable="false">${task.taskTitle}</label>
-    
-      
-     
-     
-    
-     <div id="lines"></div>
-     <button type="button" data-index=${i} onclick="eTa(this)" >otrodit</button>
-     
-     <button type="button" data-index=${i} onclick="deleteTask(this)" name="zz" class="pipi">X</button>
-     <input type="text" class="xuxu" name="cucu" required>
-     <button type="button" data-index=${i} onclick="saveTask(this)" class="btnEdit" name="pupu">V</button>
-    `;
+            <li>
+            <input type="checkbox" data-index=${i} id="task${i}" ${task.status ? 'checked' : ''} />
+            <label for="task${i}" contentEditable="false">${task.taskTitle}</label>   
+            <button type="button" data-index=${i} name="editButton"  onclick="editTask(this)">edit</button>     
+            <button type="button" data-index=${i} name="deleteButton" onclick="deleteTask(this)">
+            X</button>
+            <input type="text" id="updateTask" name="updateInput" class="hideElement" required>
+            <button type="button" data-index=${i} name="updateButton" class="hideElement" onclick="saveTask(this)" >V</button>
+            `;
     }).join('');
 }
-//<button type="button" onclick="addInput('lines')">+</button>  <button type="button"onclick="pup(this)" data-index=${i} id="task${i}">edit</button> 
-//  <label for="task${i}" >${task.status} </label>    <label id="tt${i}" contentEditable="false" data-index=${i}>${task.taskTitle}</label> 
+
 function deleteTask(e) {
-
     const taskIndex = e.dataset.index;
-
     if (taskIndex > -1) {
         tasks.splice(taskIndex, 1);
     }
-
     saveOnLocalStorage();
 }
 
 function toggleDone(e) {
-
     if (!e.target.matches('input')) return;
     const targetClick = e.target;
     const taskIndex = targetClick.dataset.index;
@@ -59,108 +56,42 @@ function toggleDone(e) {
     saveOnLocalStorage();
 }
 
-function toggle(source) {
+function toggleAllTasks(source) {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i] != source) {
             checkboxes[i].checked = source.checked;
             tasks[i].status = source.checked;
         }
-
         saveOnLocalStorage();
     }
 }
 
 function saveOnLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    populateList(tasks, taskList);
-}
-
-populateList(tasks, taskList);
-
-addTasks.addEventListener('submit', addNewTask);
-taskList.addEventListener('change', toggleDone);
-//taskList.addEventListener('change', pup);
-// markAllTasks.addEventListener('click', markAll);
-// unmarkAllTasks.addEventListener('click', unmarkAll);
-
-
-
-function pup(e) {
-
-    console.log('pup');
-
-    const taskIndex = e.dataset.index;
-    const lolo = document.getElementById("tt" + taskIndex);
-    lolo.contentEditable = "true";
-
-    console.log(lolo);
-
+    populateTaskList(tasks, taskList);
 }
 
 function saveTask(e) {
-
-
-    //save pro content editable do label
-    // const taskIndex = e.dataset.index;
-
-    //   const lolo = document.getElementById("tt"+taskIndex);
-
-    //   tasks[taskIndex].taskTitle = lolo.innerHTML;
-
-
-    const taskIndex = e.dataset.index;
-    const vv = document.getElementsByName("cucu");
-    console.log(vv[taskIndex].value);
-    if (vv[taskIndex].value == "") {
+    const taskIndex = e.dataset.index;    
+    // console.log(updateInput[taskIndex].value);
+    if (updateInput[taskIndex].value == "") {
         tasks[taskIndex].taskTitle = tasks[taskIndex].taskTitle;
     }
     else {
-        tasks[taskIndex].taskTitle = vv[taskIndex].value;
+        tasks[taskIndex].taskTitle = updateInput[taskIndex].value;
     }
     saveOnLocalStorage();
-
-
-
 }
 
-function eTa(e) {
-    const taskIndex = e.dataset.index;
-    const vv = document.getElementsByName("cucu");
-    const xx = document.getElementsByName("pupu");
-    const zz = document.getElementsByName("zz");
-   
-    vv[taskIndex].classList.remove("xuxu");
-    xx[taskIndex].classList.remove("btnEdit");
-    zz[taskIndex].classList.remove("pipi");
-    zz[taskIndex].classList.add("pipo");
+function editTask(e) {
+    const taskIndex = e.dataset.index;   
 
-    const nn = tasks[taskIndex].taskTitle;
-    console.log(nn);
-    vv[taskIndex].value = nn;
+    updateInput[taskIndex].classList.remove("hideElement");   
+    updateButton[taskIndex].classList.remove("hideElement");    
+    deleteButton[taskIndex].classList.add("hideElement");
+    editButton[taskIndex].classList.add("hideElement");
 
+    const currentTaskTitle = tasks[taskIndex].taskTitle;
+    updateInput[taskIndex].value = currentTaskTitle;
 }
-
-
-
-// var formLine = 1;
-// function addInput(divName) {
-//     console.log('enetr');
-
-//     const vv = document.getElementsByName("cucu");
-//     vv[taskIndex].classList.remove("xuxu");
-
-//     // var newdiv = document.createElement('div');
-//     // newdiv.innerHTML  = '['+formLine+']';
-//     // newdiv.innerHTML = '<input type="text" name="cucu" placeholder="Insert add ssssinput" class="taskListV"i required>';
-
-//     // // var y = taskList.getElementsByName("xuxu");
-
-//     // // console.log(y);
-//     // // y.classList.add("taskListV"); 
-//     // document.getElementById(divName).appendChild(newdiv);
-//     // formLine++;
-// }
-
-// addInput('lines');
